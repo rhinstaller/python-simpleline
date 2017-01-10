dc7e92a512dd89bc90958178a3f4955712937cd4
Jiri Konecny <jkonecny@redhat.com>
2017-01-10 14:51:58 +0100

    Add tests for App.switch_screen* methods

--- tests/schedule_screen_test.py
+++ tests/schedule_screen_test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from unittest import mock
 from simpleline.base import App, UIScreen
 
 
@@ -9,6 +10,7 @@ class ScheduleScreen_TestCase(unittest.TestCase):
         self.app = App("HelloWorld")
 
     def test_schedule_screen(self):
+        """Test App.schedule_screen() method"""
         # test if everything is set up correctly when screen is scheduled before App start
         screen = UIScreen(self.app)
         screen_argument = "test"
@@ -32,3 +34,44 @@ class ScheduleScreen_TestCase(unittest.TestCase):
         self.assertEqual(self.app._screens[0][1], screen_argument2)
         self.assertEqual(self.app._screens[0][2], self.app.NOP)
 
+    def test_switch_screen(self):
+        """Test App.switch_screen() method"""
+        old_screen = UIScreen(self.app)
+        new_screen = UIScreen(self.app)
+
+        self.app.schedule_screen(old_screen)
+        oldloop = self.app._screens[-1][2]
+        self.app.switch_screen(new_screen, "arguments")
+
+        self.assertEqual(self.app._screens[0][0], new_screen)
+        self.assertEqual(self.app._screens[0][1], "arguments")
+        self.assertEqual(self.app._screens[0][2], oldloop)
+
+    def test_switch_screen_with_return(self):
+        """Test App.switch_screen_with_return() method"""
+        old_screen = UIScreen(self.app)
+        new_screen = UIScreen(self.app)
+
+        self.app.schedule_screen(old_screen)
+        self.app.switch_screen_with_return(new_screen, "arguments")
+
+        # old screen still should be there
+        self.assertEqual(self.app._screens[0][0], old_screen)
+        # last screen in the stack should be the new one
+        self.assertEqual(self.app._screens[1][0], new_screen)
+        self.assertEqual(self.app._screens[1][1], "arguments")
+
+    @mock.patch('simpleline.base.App._do_redraw')
+    def test_switch_screen_modal(self, redraw):
+        """Test App.switch_screen_modal() method"""
+        old_screen = UIScreen(self.app)
+        new_screen = UIScreen(self.app)
+
+        self.app.schedule_screen(old_screen)
+        self.app.switch_screen_modal(new_screen, "arguments")
+
+        # old screen still should be here
+        self.assertEqual(self.app._screens[0][0], old_screen)
+        self.assertEqual(self.app._screens[1][0], new_screen)
+        self.assertEqual(self.app._screens[1][1], "arguments")
+        self.assertEqual(self.app._screens[1][2], App.START_MAINLOOP)
