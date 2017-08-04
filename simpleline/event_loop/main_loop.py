cc2cd74d4e91bf9ef51f7f42650eade1a7c0d57f
Jiri Konecny <jkonecny@redhat.com>
2017-08-04 15:52:56 +0200

    Add TicketMachine helper class
    This is synchronization mechanism.
    It is analogy for taking ticket and go to the line waiting. When
    something will mark the line to go all the tickets from this line could
    continue to work.

--- simpleline/event_loop/main_loop.py
+++ simpleline/event_loop/main_loop.py
@@ -159,3 +159,58 @@ class EventHandler(object):
     def __init__(self, callback, data):
         self.callback = callback
         self.data = data
+
+
+class TicketMachine(object):
+    """Hold signals processed by the event loop if someone wait on them.
+
+    This is useful when recursive process events will skip required signal.
+    """
+
+    def __init__(self):
+        self._lines = {}
+        self._counter = 0
+
+    def take_ticket(self, line_id):
+        """Take ticket (id) and go line (processing events).
+
+        Use `check_ticket` if you are ready to go.
+
+        :param line_id: Line where you are waiting.
+        :type line_id: Anything.
+        """
+        obj_id = self._counter
+        if line_id not in self._lines:
+            self._lines[line_id] = {obj_id: False}
+        else:
+            self._lines[line_id][obj_id] = False
+
+        self._counter += 1
+        return obj_id
+
+    def check_ticket(self, line, unique_id):
+        """Check if you are ready to go.
+
+        If True the unique_id is not valid anymore.
+
+        :param unique_id: Your id used to identify you in the line.
+        :type unique_id: int
+
+        :param line: Line where you are waiting.
+        :type line: Anything.
+        """
+        if self._lines[line][unique_id]:
+            return self._lines[line].pop(unique_id)
+
+    def mark_line_to_go(self, line):
+        """All in the `line` are ready to go.
+
+        Mark all tickets in the line as True.
+
+        :param line: Line which should processed.
+        :type line: Anything.
+        """
+        if line in self._lines:
+            our_line = self._lines[line]
+            for key in our_line:
+                our_line[key] = True
