340c0309da8bf61cfc12b0f77caa204223bccd28
Jiri Konecny <jkonecny@redhat.com>
2017-11-14 17:38:48 +0100

    Disable pylint error for doc copyright variable

--- doc/conf.py
+++ doc/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Simpleline'
-copyright = '2017, Jiri Konecny'
+copyright = '2017, Jiri Konecny'  # pylint: disable=redefined-builtin
 author = 'Jiri Konecny'
 
 # The version info for the project you're documenting, acts as replacement for
